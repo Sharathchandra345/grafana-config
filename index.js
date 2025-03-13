2fda31dbb44c235f14559a30809f877241e79a59
Sharath Chandra Chinnarigari <csharathchandra3@gmail.com>
2025-03-13 17:03:43 +0530

    Added prom client and /metrics to check metrics

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const client = require("prom-client");
 const { doSomeHeavyTask } = require("./util");
 
 const app = express();
 const PORT = 8000;
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+
+collectDefaultMetrics({ register: client.register });
 
 app.get("/", (req, res) => {
   return res.json({ message: `Hello from server` });
@@ -20,6 +25,13 @@ app.get("/slow", async (req, res) => {
       .status(500)
       .json({ status: "Error", error: "Internal Server Error" });
   }
+});
+
+//collect metrics and send to this route (uses prom client)
+app.get("/metrics", async (req, res) => {
+  res.setHeader("Content-Type", client.register.contentType);
+  const metrics = await client.register.metrics();
+  res.send(metrics);
 });
 
 app.listen(PORT, () => {
